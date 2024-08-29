44fcb09a715dff88109569f2d1932d3adee8bcdb
NatalieGuseva <gusberd81@gmail.com>
2024-08-30 00:15:09 +0500

    Calculator

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
-import java.io.IOException;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        Operations.methodOperation(input);
-        Operations.calculate(input);
         scanner.close();
+
+        if (input.matches("(.*)\\+(.*)")) {
+            System.out.print("\"" + Strings.operationPlus(input) + "\"");
+        }
+        if ((input.matches("(.*)\\-(.*)"))) {
+            System.out.print("\"" + Strings.operationMinus(input) + "\"");
+        }
+        if ((input.matches("(.*)\\*\\d+"))) {
+            System.out.print("\"" + Strings.operationMultiplication(input) + "\"");
+        }
+        if ((input.matches("(.*)\\/\\d+"))) {
+            System.out.print("\"" + Strings.operationDivision(input) + "\"");
+        }
     }
 }
